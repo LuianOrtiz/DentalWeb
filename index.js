1405c8d5b114c05a8814fcaf9a27ae17a026b81f
Luis Ortiz <luiann.ortiz@gmail.com>
2021-12-14 23:44:56 -0600

    Despues de agregar tragedias al proyecto, no llegar a nada y terminar como antes del anterior commit, se decide entregar esto

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 
 const cors = require("cors");
 const res = require('express/lib/response');
+const req = require('express/lib/request');
 app.use(
     cors({
         origin: 'http://127.0.0.1:5500',
@@ -59,32 +60,99 @@ app.post('/sendcita', async (request, response) => {
     const sql = 'INSERT INTO citas SET ?';
     console.log(request.body);
 
-    $query_paciente = "Select max(id) as id FROM pacientes";
+    //$query_paciente = "Select max(id) as id FROM pacientes";
     
-    $query_servicio = `SELECT id FROM servicios WHERE descripcion = '${request.body.servicio}'`;
+    //$query_servicio = `SELECT id FROM servicios WHERE descripcion = '${request.body.servicio}'`;
     
-    let [id_pac,id_ser] = await Promise.all([sub_consultar(request, response, $query_paciente),sub_consultar(request, response, $query_servicio)])
-
+    //let [id_pac,id_ser] = await Promise.all([sub_consultar(request, response, $query_paciente),sub_consultar(request, response, $query_servicio)])
+    var id_pac = Promise.resolve(getIdPac(request, response));
+    var id_ser = Promise.resolve(getIdSer(request, response));
+    
     const citaObj = {
         fecha_cita: request.body.fecha_cita,
         hora_cita: request.body.hora_cita,
-        id_servicio: id_ser,
-        id_paciente: id_pac,
+        id_servicio: 1,
+        id_paciente: 1,
         id_dentista: 2
     }
     
-
     connection.query(sql, citaObj, (error, rows, fields) => {
         if(error) console.log(error);
        console.log("cita creada");
     });
     
-});
+})
 
-function getIdServicio(request, response){
-    const sql = `SELECT max(id) FROM pacientes`;
-    consultar(request, response, sql);
+function getIdPac (request, response){
+    $query_paciente = "Select max(id) as id FROM pacientes";
+    var id_pac = Promise.resolve(sub_consultar(request, response, $query_paciente));
+    console.log("indi 1" +id_pac);
+    return id_pac;
 }
+
+function getIdSer(request, response){
+    $query_servicio = `SELECT id FROM servicios WHERE descripcion = '${request.body.servicio}'`;
+    var id_ser = Promise.resolve( sub_consultar(request, response, $query_servicio));
+    console.log("indi 2" +id_ser);
+    return id_ser;
+}
+
+/*
+app.post('/sendcita', async (request, response) => {
+    const sql = 'INSERT INTO citas SET ?';
+    console.log(request.body);
+
+    //var citCit = getCitas(request, response);
+    $query_paciente = "Select max(id) as id FROM pacientes";
+    //const id_pac = await sub_consultar(request, response, $query_paciente);
+    $query_servicio = `SELECT id FROM servicios WHERE descripcion = '${request.body.servicio}'`;
+    //const id_ser = await sub_consultar(request, response, $query_servicio);
+
+    function kk() {
+        Promise.resolve(Promise.all([Promise.resolve( sub_consultar(request, response, $query_servicio)),
+            Promise.resolve( sub_consultar(request, response, $query_paciente)) 
+        ])
+        ).then(([p,q]) => {
+            const citaObj = {
+                fecha_cita: request.body.fecha_cita,
+                hora_cita: request.body.hora_cita,
+                id_servicio: p,
+                id_paciente: q,
+                id_dentista: 2
+            }
+            console.log("papa" +citaObj);
+            connection.query(sql, citaObj, (error, rows, fields) => {
+                if(error) console.log(error);
+                console.log("cita creada");
+            })
+        })
+    }
+
+    const getCitas = async (request, response) => {
+        var valorCit = []
+        $query_paciente = "Select max(id) as id FROM pacientes";
+        valorCit[0] = await sub_consultar(request, response, $query_paciente);
+        console.log("valorCit 2:" +valorCit[0]);
+        $query_servicio = `SELECT id FROM servicios WHERE descripcion = '${request.body.servicio}'`;
+        valorCit[1] = await sub_consultar(request, response, $query_servicio);
+        console.log("valorCit 2:" +valorCit[1]);
+    
+        return citaObj = {
+            fecha_cita: request.body.fecha_cita,
+            hora_cita: request.body.hora_cita,
+            id_servicio: await Promise.resolve( sub_consultar(request, response, $query_servicio)),
+            id_paciente: await Promise.resolve( sub_consultar(request, response, $query_paciente)),
+            id_dentista: 2
+        }
+    
+    }
+    
+        kk()
+
+})
+*/
+
+
 
 const consultar = async (req,res,$query) => {
     connection.query($query, await function(err,rows,flieds){
